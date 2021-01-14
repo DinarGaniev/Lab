78e8ac21943e0a440598323e9a9d04d07f5ace4d
DinarGaniev <dinar0607s@gmail.com>
2021-01-14 15:07:16 +0300

    Lab6_1

---
+++ Dinar_Ganiev_Lab_6_1_201_351/Dinar_Ganiev_Lab_6_1_201_351.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+#include<string>
+#include<stack>
+int main()
+{
+	std::string str;
+	std::cout << "Input str: \n";
+	std::cin >> str;
+	std::stack<char> strin;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '(' or str[i] == '[' or str[i] == '{')
+		{
+			strin.push(str[i]); // если встречается какая-либо скобка, то мы добавляем её в стек
+		}
+		if (str[i] == ')')
+		{
+			if (strin.empty())
+			{
+				std::cout << "NO";
+				return 0;
+			}
+		}
+		else if (str[i] == ')' and strin.top() == '(') { // встречена закрfывающая скобка, то проверяется верхушка стека, если там круглая скобка, то удаляем ее
+			strin.pop();
+		}
+		else if (str[i] == ')' and strin.top() != '(') { // если последовательность неправильная, то работа программы завершается
+			  std::cout <<"NO";
+			  return 0;
+		}
+		if (str[i] == ']')
+		{
+			if (strin.empty())
+			{
+				std::cout << "NO";
+				return 0;
+			}
+		}
+		else if (str[i] == ']' and strin.top() == '[') {
+			strin.pop();
+		}
+		else if (str[i] == ']' and strin.top() != '[') {
+			   std::cout << "NO";
+			   return 0;
+		}
+		if (str[i] == '}')
+		{
+			if (strin.empty())
+			{
+				std::cout << "NO";
+				return 0;
+			}
+		}
+		else if (str[i] == '}' and strin.top() == '{') {
+			strin.pop();
+		}
+		else if (str[i] == '}' and strin.top() != '{') {
+			  std::cout << "NO";
+			  return 0;
+		}
+	}
+	strin.empty() ? std::cout << "Yes" :
+		std::cout << "No";
+	return 0;
+}
+
