4c16492c5a83c10ad13a457724890204e57d4ecb
DinarGaniev <dinar0607s@gmail.com>
2021-01-14 15:01:24 +0300

    Lab6_3

--- Dinar_Ganiev_201_351_Lab_6_3/Dinar_Ganiev_201_351_Lab_6_3.cpp
+++ Dinar_Ganiev_201_351_Lab_6_3/Dinar_Ganiev_201_351_Lab_6_3.cpp
@@ -1,32 +1,46 @@
 ﻿
 #include <iostream>
 #include <queue>
-bool Proverka(int num)
+int min(int a1, int a2, int a3)// функция поиска минимального значения от трех элементов
 {
-	if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0)
+	if ((a1 <= a2) and (a1 <= a3))
 	{
-		return true;
+		return a1;
 	}
-	else return false;
+	if ((a2 <= a1) and (a2 <= a3))
+	{
+		return a2;
+	}
+	if ((a3 <= a2) and (a3 <= a1))
+	{
+		return a3;
+	}
 }
 int main()
 {
-	std::queue<int>q1;
+	int element = 1;
 	int n;
+	std::queue<int> q2, q3, q5;
+	std::cout << "Input n" << std::endl;
 	std::cin >> n;
-	int i = 1;
-
-	while (q1.size() < n) // цикл добавления элементов в queue
+	for (int i = 0; i < n; i++)
 	{
-		i++;
-		if (Proverka(i)) //условие добавления элементов в queue
+		std::cout << element << std::endl;
+		q2.push(element * 2);
+		q3.push(element * 3);
+		q5.push(element * 5);
+		element = min(q2.front(), q3.front(), q5.front());
+		if (q2.front() == element)
 		{
-			q1.push(i);
+			q2.pop();
 		}
-	}
-	while (!q1.empty())
-	{
-		std::cout << q1.front()<< std::endl;
-		q1.pop();
+		if (q3.front() == element)
+		{
+			q3.pop();
+		}
+		if (q5.front() == element)
+		{
+			q5.pop();
+		}
 	}
 }
